3b38bcabdf963553c71ac6b29d1ce41e127a9992
asytka <asyt2203@gmail.com>
2024-05-03 00:57:44 +0300

    code refactoring

--- signal-db.js
+++ signal-db.js
@@ -31,34 +31,22 @@ db.pragma(`key = "x'${getDBKey()}'"`);
 
 const conv = db.prepare(`SELECT id, name FROM conversations WHERE type = 'group' OR type = 'private'`);
 
-const msgRes = [];
 
 for (const convi of conv.iterate()) {
     const msg = db.prepare(`SELECT type, body, sent_at FROM messages WHERE ConversationId = ?`);
-    const msgsForConv = msg.all(convi.id); // Use parameterized query to avoid SQL injection
+    const msgsForConv = msg.all(convi.id); 
     for (const msg of msgsForConv) msg.sent_at = getLocalTime(msg.sent_at);
-    //msgsForConv.sent_at = getLocalTime(msgsForConv.sent_at);
+
     const jsonData = JSON.stringify(msgsForConv);
     fs.writeFileSync(`messages/${convi.name}.json`, jsonData, 'utf8');
-    msgRes.push(...msgsForConv);
 }
 
-//console.log(msgRes);
 
-const convRes = conv.all();
-// Convert the messages array to JSON
-const jsonData = JSON.stringify(convRes);
 
-// Write JSON data to a file
-//fs.writeFileSync('messages/messages.json', jsonData, 'utf8');
-
-// Close the database connection
 db.close();
 
-// Log success message
 console.log(`Messages saved to ${process.cwd()}\\messages folder`);
 
-// Wait for user input before exiting
 require('readline')
     .createInterface(process.stdin, process.stdout)
     .question("Press [Enter] to exit...", function(){
