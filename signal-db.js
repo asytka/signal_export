5423865d3e5b9be6e85116f68a8600ad1e3954cb
asytka <asyt2203@gmail.com>
2024-05-12 22:54:47 +0300

    fixed text issues

--- signal-db.js
+++ signal-db.js
@@ -1,12 +1,9 @@
 const os = require('os');
 const fs = require('fs');
 const path = require('path');
-const SQL = require('./node_modules/@signalapp/better-sqlite3');
+const SQL = require('@signalapp/better-sqlite3');
 
-const folderPath = ['./messages/json', './messages/text'];
-
-for (var i in folderPath) fs.mkdir(folderPath[i], () => {});
-
+fs.mkdir("./messages", () => {});
 
 function getFolderPath() {
     if (process.platform === 'win32') {
@@ -30,6 +27,10 @@ function getLocalTime(timestamp) {
     return date.toLocaleString();
 }
 
+function formatText(str) {
+    return str.replace(/\u2068/g, '').replace(/\u2069/g, '')
+}
+
 const db = SQL(getDBPath(), { readonly: true });
 
 db.pragma(`key = "x'${getDBKey()}'"`);
@@ -42,19 +43,18 @@ for (const conv_i of conv_stmt.iterate()) {
     const msg = msg_stmt.all(conv_i.id); 
     for (const msg_i of msg) msg_i.sent_at = getLocalTime(msg_i.sent_at);
 
-    const jsonData = JSON.stringify(msg);
     var Data = `Чат: ${conv_i.name} (${conv_i.type}) \n\n`;
     for (const msg_i of msg) {
         if (msg_i.type == 'group-v2-change') { 
             Data += `Створення групи: ${msg_i.sent_at}\n`;
         }
         else {
-            Data += `Час: ${msg_i.sent_at} \nТекст: ${msg_i.body} \nВідправник ${msg_i.type}\n`;
+            Data += `Час: ${msg_i.sent_at} \nТекст: ${msg_i.body} \nТип повідомлення: ${msg_i.type = (msg_i.type == "outgoing") ? "Вихідний" : "Вхідний"}\n`;
         }
         Data += "\n";
     }
-    fs.writeFileSync(`messages/text/${conv_i.name}.txt`, Data, 'utf8');
-    fs.writeFileSync(`messages/json/${conv_i.name}.json`, jsonData, 'utf8');
+
+    fs.writeFileSync(`messages/${formatText(JSON.stringify(conv_i.name).replace(/"/g, ''))}.txt`, formatText(Data), 'utf8');
 
 }
 
